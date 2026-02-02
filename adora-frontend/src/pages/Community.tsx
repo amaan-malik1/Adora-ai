1aa0ca580f152b8f6434a8655701fbbf1757e924
amaan-malik1 <amikm2077@gmail.com>
2026-02-02 22:00:22 +0530

    Community page build

--- adora-frontend/src/pages/Community.tsx
+++ adora-frontend/src/pages/Community.tsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import type { Project } from "../types";
+import { dummyGenerations } from "../assets/assets";
+import { Loader2Icon } from "lucide-react";
+import ProjectCard from "../components/ProjectCard";
 
 const Community = () => {
-  return (
-    <div>Community</div>
-  )
-}
+  const [projects, setProjects] = useState<Project[]>([]);
+  const [loading, setLoading] = useState(true);
 
-export default Community
+  const fetchProjects = async () => {
+    setTimeout(() => {
+      setProjects(dummyGenerations);
+      setLoading(false);
+    }, 3000);
+  };
+
+  useEffect(() => {
+    fetchProjects();
+  }, []);
+
+  return loading ? (
+    <div className="flex items-center justify-center min-h-screen">
+      <Loader2Icon className="size-7 animate-spin text-indigo-400" />
+    </div>
+  ) : (
+    <div className="min-h-screen text-white p-6 md:p-12 my-28">
+      <div className="max-w-6xl mx-auto">
+        <header>
+          <h1>Community</h1>
+          <p>Explore projects created by the community with Adora-Ai</p>
+        </header>
+        {/* projects list */}
+        <div className="columns-1 sm:columns-2 lg:columns-3 gap-4 mt-8">
+          {projects.map((project) => (
+            <ProjectCard
+              gen={project}
+              key={project.id}
+              setGeneration={setProjects}
+              forCommunity={true}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Community;
