b8d08f141afc109b8e1ee77d95ef411c7a43e82c
amaan-malik1 <amikm2077@gmail.com>
2026-02-02 23:27:39 +0530

    my-generations page

--- adora-frontend/src/pages/MyGeneration.tsx
+++ adora-frontend/src/pages/MyGeneration.tsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import type { Project } from "../types";
+import { dummyGenerations } from "../assets/assets";
+import { Loader2Icon } from "lucide-react";
+import ProjectCard from "../components/ProjectCard";
 
 const MyGeneration = () => {
-  return (
-    <div>MyGeneration</div>
-  )
-}
+  const [generations, setGenerations] = useState<Project[]>([]);
+  const [loading, setLoading] = useState(true);
 
-export default MyGeneration
+  const fetchMyGenerations = async () => {
+    setTimeout(() => {
+      setGenerations(dummyGenerations);
+      setLoading(false);
+    }, 3000);
+  };
+
+  useEffect(() => {
+    fetchMyGenerations();
+  }, []);
+
+  return loading ? (
+    <div className="flex items-center justify-center min-h-screen">
+      <Loader2Icon className="size-7 animate-spin text-indigo-400" />
+    </div>
+  ) : (
+    <div className="min-h-screen text-white p-6 md:p-12 my-28">
+      <div className="max-w-6xl mx-auto">
+        <header className="mb-12">
+          <h1 className="text-3xl md:text-4xl font-semibold mb-4">My Generations</h1>
+          <p className="text-gray-400">View and manage your generated projects</p>
+        </header>
+        {/* projects list */}
+        <div className="columns-1 sm:columns-2 lg:columns-3 gap-4 mt-8">
+          {generations.map((project) => (
+            <ProjectCard
+              gen={project}
+              key={project.id}
+              setGeneration={setGenerations}
+              forCommunity={false}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MyGeneration;
