85509cfbf3a9078ae0dd7cbbcc5cde5cdb043f01
amaan-malik1 <amikm2077@gmail.com>
2026-02-03 12:00:00 +0530

    my-generations page

--- adora-frontend/src/components/ProjectCard.tsx
+++ adora-frontend/src/components/ProjectCard.tsx
@@ -52,26 +52,26 @@ const ProjectCard = ({
           )}
         </div>
 
-        {/* satus badge */}
-        <div className="absolute flex left-3 top-3 gap-2 items-center">
+        {/* status badge */}
+        {/* <div className="absolute flex left-3 top-3 gap-2 items-center">
           {/* for generation */}
-          {gen.isGenerating && (
+        {/* {gen.isGenerating && (
             <span className="text-xs px-2 py-1 bg-yellow-600/30 rounded-full">
               Generating{" "}
             </span>
           )}
 
           {/* for is published */}
-          {gen.isPublished && (
+        {/* {gen.isPublished && (
             <span className="text-xs px-2 py-1 bg-green-600/30 rounded-full">
               Published{" "}
             </span>
           )}
-        </div>
+        </div> */}
 
         {/* source images */}
-        <div className="absolute right-3 bottom-3">
-          {/* <img
+        {/* <div className="absolute right-3 bottom-3">
+          <img
             src={gen.uploadedImage?.[0] || ""}
             alt="product"
             className="w-16 h-16 object-cover rounded-full animate-float"
@@ -81,17 +81,12 @@ const ProjectCard = ({
             alt="model"
             className="w-16 h-16 object-cover rounded-full animate-float -ml-8"
             style={{ animationDelay: "3s" }}
-          /> */}
-          {gen.uploadedImage?.[0] && (
-            <img
-              src={gen.uploadedImage[0]}
-              className="w-16 h-16 object-cover rounded-full animate-float"
-            />
-          )}
-        </div>
+          />
+        </div> */}
 
         {/* details  */}
         <div className="p-4">
+          {/* product name, date, aspect ratio */}
           <div className="flex items-start justify-between gap-4">
             <div className="flex-1">
               <h3 className="font-medium text-lg mb-1">{gen.productName}</h3>
@@ -113,6 +108,26 @@ const ProjectCard = ({
               </div>
             </div>
           </div>
+
+          {/* product description */}
+          {gen.productDescription && (
+            <div className="mt-3">
+              <p className="text-xs text-gray-400 mb-1">Description</p>
+              <div className="text-sm text-gray-300 bg-white/3 p-2 rounded-md wrap-break-word">
+                {gen.productDescription}
+              </div>
+            </div>
+          )}
+
+          {/* user prompt */}
+          {gen.userPrompt && (
+            <div className="mt-3">
+              <div className="text-xs text-gray-300">
+                User Prompt
+                {gen.userPrompt}
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </div>
